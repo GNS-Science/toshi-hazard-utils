95c7c2ab51314ba2d77089d004c19d4c82b17d70
Chris Chamberlain <chrisbc@artisan.co.nz>
2022-08-02 15:55:41 +1200

    updated CodedLocation; WIP on main features

--- tests/features/test_main_features.py
+++ tests/features/test_main_features.py
@@ -18,13 +18,13 @@ GRID_02 = load_grid('NZ_0_2_NB_1_1')
 VS30S = [250, 350, 450]
 IMTS = ['PGA', 'SA(0.5)']
 AGGS = ['mean', '0.10']
-LOCS = [CodedLocation(*o) for o in GRID_02[20:50]]
+LOCS = [CodedLocation(*o, 0.001) for o in GRID_02[20:50]]
+N_LVLS = 29
 
 
 def build_hazard_aggregation_models():
 
-    n_lvls = 29
-    lvps = list(map(lambda x: model.LevelValuePairAttribute(lvl=x / 1e3, val=(x / 1e6)), range(1, n_lvls)))
+    lvps = list(map(lambda x: model.LevelValuePairAttribute(lvl=x / 1e3, val=(x / 1e6)), range(1, N_LVLS)))
     for (loc, vs30, agg) in itertools.product(LOCS, VS30S, AGGS):
         for imt, val in enumerate(IMTS):
             yield model.HazardAggregation(
@@ -44,42 +44,34 @@ class HighLevelHazard(unittest.TestCase):
             for item in build_hazard_aggregation_models():
                 batch.save(item)
 
-    @unittest.skip("not implemented")
     def test_list_hazard_data_available_for_grid_location(self):
-
-        loc = LOCS[0].downsample(0.001)
+        loc = LOCS[0]
         myhazards = list(hazard_report([HAZARD_MODEL_ID], locations=[loc.code]))
         print(myhazards)
+
         result = myhazards[0]
-
         self.assertTrue(isinstance(result, ToshiHazardReport))
+        self.assertEqual(len(myhazards), 1)
         self.assertEqual(result.loc, loc)
-        # self.assertEqual(myhazards[0], ToshiHazardReport(CodedLocation(-45, 172), 400, imts='PGA', agg='0.5', lvps=[(0.1, 0.001), (0.2, 0.0002)]))  # noqa
-        assert 0
+        self.assertEqual(set(IMTS), result.imts)
+        self.assertEqual(set(AGGS), result.aggs)
+        self.assertEqual(set(VS30S), result.vs30s)
+        self.assertEqual(set(lvl / 1e3 for lvl in range(1, N_LVLS)), result.levels)
 
-    @unittest.skip("not implemented")
     def test_get_nearest_hazard_for_an_arbitrary_location(self):
-
-        gridloc = random.choice(LOCS)  # [random.randint(0,len(LOCS))
+        gridloc = random.choice(LOCS)
         print(f'gridloc {gridloc}')
 
         off_lat = round(gridloc.lat + random.randint(0, 9) * 0.01, 3)
         off_lon = round(gridloc.lon + random.randint(0, 9) * 0.01, 3)
-        somewhere_off_grid = CodedLocation(off_lat, off_lon)
+        somewhere_off_grid = CodedLocation(off_lat, off_lon, 0.001)
         nearest_grid = somewhere_off_grid.downsample(0.2)
 
         print(f'somewhere_off_grid {somewhere_off_grid}')
         print(f'nearest_grid {nearest_grid}')
 
-        self.assertEqual(gridloc, CodedLocation(nearest_grid.lat, nearest_grid.lon))
-        self.assertTrue(CodedLocation(nearest_grid.lat, nearest_grid.lon) in LOCS)
-
-        myhazards = list(hazard_report([HAZARD_MODEL_ID], locations=[nearest_grid.downsample(0.001).code]))
-        print(myhazards)
-        self.assertEqual(
-            myhazards[0],
-            ToshiHazardReport(CodedLocation(-45, 172), 400, imt='PGA', agg='0.5', lvps=[(0.1, 0.001), (0.2, 0.0002)]),
-        )
+        self.assertEqual(gridloc, CodedLocation(nearest_grid.lat, nearest_grid.lon, 0.001))
+        self.assertTrue(CodedLocation(nearest_grid.lat, nearest_grid.lon, 0.001) in LOCS)
 
 
 @unittest.skip("not implemented")
--- toshi_hazard_utils/hazard/hazard.py
+++ toshi_hazard_utils/hazard/hazard.py
@@ -1,18 +1,18 @@
 """Helpers for obtaining hazard data from Toshi Hazard Store."""
-import dataclasses
+from dataclasses import dataclass, field
 from typing import Dict, Iterable, Set  # List, Set, Tuple
 
 from nzshm_common.location.code_location import CodedLocation
 from toshi_hazard_store import query_v3
 
 
-@dataclasses.dataclass
+@dataclass
 class ToshiHazardReport:
     loc: CodedLocation
-    vs30s: Set[float] = set()
-    imts: Set[str] = set()
-    aggs: Set[str] = set()
-    levels: Set[Iterable[float]] = set()
+    vs30s: Set[float] = field(default_factory=set)
+    imts: Set[str] = field(default_factory=set)
+    aggs: Set[str] = field(default_factory=set)
+    levels: Set[Iterable[float]] = field(default_factory=set)
 
 
 def hazard_report(hazard_model_ids: Iterable[str], locations: Iterable[CodedLocation]) -> Iterable[ToshiHazardReport]:
@@ -20,7 +20,7 @@ def hazard_report(hazard_model_ids: Iterable[str], locations: Iterable[CodedLoca
 
     loc_reports: Dict[CodedLocation, ToshiHazardReport] = {}
     for res in query_v3.get_hazard_curves(locations, vs30s=[], hazard_model_ids=hazard_model_ids, imts=[]):
-        loc = CodedLocation(res.lat, res.lon)
+        loc = CodedLocation(res.lat, res.lon, 0.001)
 
         if loc not in loc_reports.keys():
             loc_reports[loc] = ToshiHazardReport(loc)  # , {[0]}, {[]}, {[]}, {[]})  # noqa
@@ -29,7 +29,8 @@ def hazard_report(hazard_model_ids: Iterable[str], locations: Iterable[CodedLoca
         report.imts.add(res.imt)
         report.vs30s.add(res.vs30)
         report.aggs.add(res.agg)
-        report.levels.add([v.lvl for v in res.values])
+        for v in res.values:
+            report.levels.add(v.lvl)
 
-    for report in loc_reports:
+    for loc, report in loc_reports.items():
         yield report
